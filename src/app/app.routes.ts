f816d582867aa73d4a43243c728916dfdc8110f0
Arnold Ekechi <chinonsoekechi@gmail.com>
2026-01-06 11:39:42 +0000

    Day 1: Angular 18 standalone setup with app shell + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AppShellComponent } from './core/layouts/app-shell/app-shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/dashboard/dashboard-page/dashboard-page.component')
+            .then(m => m.DashboardPageComponent),
+      },
+      {
+        path: 'users',
+        loadComponent: () =>
+          import('./features/users/users-page/users-page.component')
+            .then(m => m.UsersPageComponent),
+      },
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
+
---
+++ src/app/core/layouts/app-shell/app-shell.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-app-shell',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './app-shell.component.html',
+  styleUrl: './app-shell.component.scss'
+})
+export class AppShellComponent {
+
+}
